c4bbcdbf2471833a1fa38d06bb3c4dc2e19812b8
Jitendra Kumar Pandit <118817891+Jitendra7740@users.noreply.github.com>
2022-12-03 22:31:06 +0530

    Add files via upload

---
+++ class32-chapter2 exercise3-CipherSchools.py
@@ -0,0 +1,16 @@
+
+#take two comma seperated inputs from the user
+# 1. User's name
+# 2. single character
+ 
+# output - 2 lines to be print in output
+# 1.) User's name length
+# 2.) Count the character that user inputed (bonus : case insensitive count)
+  
+word,single_character=input("input should be comma seperated: " ).split(",")
+length=len(word)
+small_word=word.lower()
+small_char=single_character.lower()
+times=small_word.count(small_char)
+print(f"length of the word is {length}")
+print(f"number of count of {single_character} is {times}")
